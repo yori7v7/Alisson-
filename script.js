c90c804c8e4cfaa5fb875a6f7eddb8b4e37d949c
Diego Castelán <dycs1804@gmail.com>
2025-04-17 02:06:49 -0600

    Final

--- script.js
+++ script.js
@@ -210,13 +210,21 @@ const cositasData=[
   {type:"video",youtubeId:"JLOUxj9kuwU"},
   {type:"video",youtubeId:"W9GY6RV_Vbg"},
   {type:"final",text:`
-      ❤️ 17/04/2025 ❤️
+  ❤️ 17/04/2025 ❤️
 
-      Mi amada Alisson:
+  Mi amada Alisson:
 
-      Hemos llegado al final de esta pequeña gran sorpresa. ...
-      Con todo mi amor y esperando que algún día volvamos a coincidir,
-      Diego Yorel Castelán Silva ❤️
+  Hemos llegado al final de esta pequeña gran sorpresa. Antes que nada, quiero agradecerte por existir en mi vida y por cada sonrisa que me has regalado. También quiero pedirte perdón por todos mis errores, por las veces en que mis acciones o mis palabras te lastimaron y, sobre todo, por no haberte cuidado como merecías.
+
+  Hoy, en nuestro aniversario, quiero que sepas que mi corazón sigue atado al tuyo. Si de verdad estás segura de que no sientes nada ya por mí, entiendo tu decisión y la respeto. Pero si en algún momento, durante este recorrido, sentiste una pizca de ese amor que antes nos unía, para mí es la prueba de que nuestro amor persiste. Te quiero recuperar a como dé lugar, pero también sabré esperar lo que haga falta, porque confío en que lo que vivimos no fue en vano.
+
+  Te deseo lo mejor, aunque sea lejos de mí. Sin embargo, no negaré que mi ilusión sigue siendo que un día nos reencontremos y volvamos a sonreír juntos. Perdón por mis estupideces, por las veces en que te incomodé o te hice enojar. Me arrepiento de haber arruinado algo tan bello que teníamos, y aunque me digas que no, yo seguiré confiando en que nuestro amor fue (y es) real.
+
+  Gracias por cada momento que compartiste conmigo, por tu dulzura, por tu alegría, por tu manera de ver la vida y hacerme sentir tan especial. Pase lo que pase, siempre tendrás un pedazo de mi corazón.
+
+  Con todo mi amor y esperando que algún día volvamos a coincidir,  
+
+  Diego Yorel Castelán Silva. ❤️
     `,videoId:"ym0gklzDw3c"}
 ];
 let cositaActual=0;
